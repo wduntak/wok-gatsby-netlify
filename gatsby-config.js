e24ba2c8217c05806816033eba9274e65d355cc0
wduntak <wduntak@gmail.com>
2020-07-02 08:47:14 -0400

    Initialize gatsby starter and setup netlify cms config

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          `gatsby-plugin-netlify-cms`,
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
